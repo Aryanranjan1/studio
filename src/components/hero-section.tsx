803121868bfc3ba879394502c4d499972d98d505
Aryan Ranjan <ashu13112003@gmail.com>
2025-09-14 18:36:24 +0000

    use this for hero section video https://www.pexels.com/download/video/31

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -52,7 +52,7 @@ export function HeroSection() {
                                 muted
                                 playsInline
                                 className="size-full object-cover opacity-50 invert dark:opacity-35 dark:invert-0 dark:lg:opacity-75"
-                                src="https://ik.imagekit.io/lrigu76hy/tailark/dna-video.mp4?updatedAt=1745736251477"></video>
+                                src="https://www.pexels.com/download/video/3163534/"></video>
                         </div>
                     </div>
                 </section>
