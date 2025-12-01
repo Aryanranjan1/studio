0c94de6246e6b5bf1b9a7ba2751e49c809f91bb2
Aryan Ranjan <ashu13112003@gmail.com>
2025-12-01 22:14:30 +0000

    bring it to right div name stack group bottom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@
 export default function Home() {
   return (
     <main className="flex w-full flex-col md:min-h-screen">
-      <div className="flex flex-1">
+      <div className="flex flex-1 justify-end">
         <div className="w-[27%] bg-purple-500">
-          {/* div1 */}
+          {/* stack group bottom */}
         </div>
       </div>
     </main>
