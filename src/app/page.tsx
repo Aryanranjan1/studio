de16014ce14ebf6ed53ed1d40725c2f11d831476
Aryan Ranjan <ashu13112003@gmail.com>
2025-12-01 22:13:52 +0000

    27% of width from left will be first div make it pruple for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 
 export default function Home() {
   return (
-    <main className="flex w-full flex-col p-4 md:min-h-screen">
-      {/* We will build the bento grid here */}
+    <main className="flex w-full flex-col md:min-h-screen">
+      <div className="flex flex-1">
+        <div className="w-[27%] bg-purple-500">
+          {/* div1 */}
+        </div>
+      </div>
     </main>
   );
 }
