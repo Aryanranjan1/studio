f953ed6dc81488f09dbbe92aa42e78a9983b9228
Aryan Ranjan <ashu13112003@gmail.com>
2025-12-01 22:03:26 +0000

    we will make a bento box for hero section make it as i say okay ?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <main className="flex flex-1 items-center justify-center p-4 md:p-8">
-      {/* The new hero section will be built here */}
+    <main className="flex min-h-[calc(100vh-5rem)] w-full flex-col items-center justify-center p-4">
+      {/* We will build the bento grid here */}
     </main>
   );
 }
