984b7424185e58eaa98fe2f7927da9f2a0648cb6
Aryan Ranjan <ashu13112003@gmail.com>
2025-12-01 22:21:53 +0000

    now new dive white colour

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,10 @@
 export default function Home() {
   return (
     <main className="flex w-full flex-col md:min-h-screen">
-      <div className="flex flex-1 justify-end">
+      <div className="flex flex-1 justify-between">
+        <div className="h-[75%] w-[15%] bg-white">
+          {/* new white div */}
+        </div>
         <div className="w-[27%] bg-purple-500">
           {/* stack group bottom */}
         </div>
