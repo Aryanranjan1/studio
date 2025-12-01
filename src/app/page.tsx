ec4a8c886976a8460c8f274aa5dc9c60e34fff37
Aryan Ranjan <ashu13112003@gmail.com>
2025-12-01 22:05:15 +0000

    first hero section logic i n desktop hero section will occupy one full s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-[calc(100vh-5rem)] w-full flex-col items-center justify-center p-4">
+    <main className="flex w-full flex-col items-center justify-center p-4 md:min-h-screen">
       {/* We will build the bento grid here */}
     </main>
   );
