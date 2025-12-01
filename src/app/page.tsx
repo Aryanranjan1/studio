470a5153521e2e7184de6a8cf9a527663e8a2405
Aryan Ranjan <ashu13112003@gmail.com>
2025-12-01 22:09:48 +0000

    first we  make divs use colour temporarliy ill mname the div also group

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <main className="flex w-full flex-col items-center justify-center p-4 md:min-h-screen">
+    <main className="flex w-full flex-col p-4 md:min-h-screen">
       {/* We will build the bento grid here */}
     </main>
   );
