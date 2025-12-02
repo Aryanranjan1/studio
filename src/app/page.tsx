dac78104e957f3cceb58b43aeee0f8badb693096
Aryan Ranjan <ashu13112003@gmail.com>
2025-12-02 04:54:11 +0000

    30

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,16 @@ import { HeroSvg } from "@/components/hero-svg";
 
 export default function Home() {
   return (
-    <main className="flex h-[calc(100vh-3.5rem)] w-full flex-row items-start justify-start bg-background p-0 overflow-hidden">
+    <main className="relative flex h-[calc(100vh-3.5rem)] w-full flex-col items-start justify-start bg-background p-0 overflow-hidden">
       <div className="h-full w-[75%] flex flex-col" style={{ marginTop: '-120px' }}>
         <HeroSvg />
 
         {/* Mid Section */}
         <div id="mid-section" className="flex-grow flex items-center p-4">
-            <div className="w-[25%] aspect-square bg-card rounded-lg">
+            <div className="w-[30%] aspect-square bg-card rounded-lg">
                 {/* Content for the first element */}
             </div>
-            <div className="flex-1 h-full ml-2 bg-card rounded-lg" style={{ maxHeight: 'calc(25vw)' }}>
+            <div className="flex-1 h-full ml-2 bg-card rounded-lg" style={{ maxHeight: 'calc(30vw)' }}>
                 {/* Content for the second element */}
             </div>
         </div>
