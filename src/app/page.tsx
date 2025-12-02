4743b75f08ebe8722bbf3307321c8f8d3ba98258
Aryan Ranjan <ashu13112003@gmail.com>
2025-12-02 03:17:04 +0000

    now make a rectangle box full fieght of her section and width 30% of her

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="relative flex h-[calc(100vh-3.5rem)] w-full flex-col items-start justify-start bg-background p-0 overflow-hidden">
-      <div className="absolute left-0 top-0 w-full lg:w-[75%] lg:h-[75vh] lg:-mt-[10vh]">
+      <div className="absolute left-0 top-0 w-full lg:w-[70%] lg:h-[75vh] lg:-mt-[10vh]">
         <svg
           width="100%"
           height="100%"
@@ -28,6 +28,7 @@ export default function Home() {
           </g>
         </svg>
       </div>
+      <div className="absolute right-0 top-0 h-full w-[30%] bg-purple-500"></div>
     </main>
   );
 }
