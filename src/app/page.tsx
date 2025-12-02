819cd8cfd778d0b04327bd428e234c2c31fd15c4
Aryan Ranjan <ashu13112003@gmail.com>
2025-12-02 04:52:23 +0000

    actually its perfect now thank you now we move on from thsi and create 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,19 @@ import { HeroSvg } from "@/components/hero-svg";
 export default function Home() {
   return (
     <main className="flex h-[calc(100vh-3.5rem)] w-full flex-row items-start justify-start bg-background p-0 overflow-hidden">
-      <div className="h-full w-[75%]" style={{ marginTop: '-120px' }}>
+      <div className="h-full w-[75%] flex flex-col" style={{ marginTop: '-120px' }}>
         <HeroSvg />
+
+        {/* Mid Section */}
+        <div id="mid-section" className="flex-grow flex items-center p-4">
+            <div className="w-[20%] aspect-square bg-card rounded-lg">
+                {/* Content for the first element */}
+            </div>
+            <div className="flex-1 h-full ml-2 bg-card rounded-lg" style={{ maxHeight: 'calc(20vw)' }}>
+                {/* Content for the second element */}
+            </div>
+        </div>
+
       </div>
       <div className="h-full w-[25%] bg-purple-500 rounded-l-xl">
         {/* Content for the right side div goes here */}
