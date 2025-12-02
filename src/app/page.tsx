c3cbc01997c886a683714b92618a58e5ab60f282
Aryan Ranjan <ashu13112003@gmail.com>
2025-12-02 06:09:07 +0000

    margin between mid group and svg group 8px

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,17 @@ import { HeroSvg } from '@/components/hero-svg';
 
 export default function Home() {
   return (
-    <main className="flex w-full flex-col items-stretch justify-start bg-background p-0 lg:flex-row lg:h-screen">
+    <main className="flex flex-col items-stretch justify-start bg-background p-0 lg:h-screen lg:flex-row">
       <div className="flex w-full flex-col lg:w-[75%]">
         {/* SVG Container (grows to fill space) */}
-        <div className="flex-1 overflow-hidden -mt-[75px] lg:-mt-[100px]">
+        <div className="flex-1 overflow-hidden lg:-mt-[100px] mb-2">
           <HeroSvg />
         </div>
 
         {/* Mid Section */}
         <div
           id="mid-section"
-          className="mr-2 flex items-stretch pl-7 pr-2 pb-2"
+          className="mr-2 flex items-stretch pl-7 pr-2 pb-2 lg:mr-0 lg:pl-0"
         >
           <div className="h-32 w-[30%] rounded-lg bg-card">
             {/* Content for the first element */}
@@ -22,8 +22,10 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="mt-2 h-64 w-full rounded-lg bg-purple-500 lg:mt-0 lg:h-auto lg:w-[25%] lg:rounded-l-xl lg:rounded-r-none">
-        {/* Content for the right side div goes here */}
+      <div className="relative w-full lg:w-[25%]">
+        <div className="absolute inset-0 m-2 mt-4 rounded-lg bg-purple-500 lg:m-0 lg:rounded-l-xl lg:rounded-r-none">
+          {/* Content for the right side div goes here */}
+        </div>
       </div>
     </main>
   );
