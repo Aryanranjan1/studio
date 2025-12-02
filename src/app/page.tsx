c643b934462577bde6ec13e5a22d7326e6ae6d61
Aryan Ranjan <ashu13112003@gmail.com>
2025-12-02 06:09:53 +0000

    left margin on mid group only left margin 15px

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,14 @@ export default function Home() {
     <main className="flex flex-col items-stretch justify-start bg-background p-0 lg:h-screen lg:flex-row">
       <div className="flex w-full flex-col lg:w-[75%]">
         {/* SVG Container (grows to fill space) */}
-        <div className="flex-1 overflow-hidden lg:-mt-[100px] mb-2">
+        <div className="flex-1 lg:-mt-[100px] mb-2">
           <HeroSvg />
         </div>
 
         {/* Mid Section */}
         <div
           id="mid-section"
-          className="mr-2 flex items-stretch pl-7 pr-2 pb-2 lg:mr-0 lg:pl-0"
+          className="flex items-stretch pb-2 ml-[15px] pr-2 lg:pr-0"
         >
           <div className="h-32 w-[30%] rounded-lg bg-card">
             {/* Content for the first element */}
@@ -23,7 +23,7 @@ export default function Home() {
         </div>
       </div>
       <div className="relative w-full lg:w-[25%]">
-        <div className="absolute inset-0 m-2 mt-4 rounded-lg bg-purple-500 lg:m-0 lg:rounded-l-xl lg:rounded-r-none">
+        <div className="absolute inset-0 m-2 mt-4 rounded-lg bg-purple-500 lg:m-0 lg:mt-0 lg:rounded-l-xl lg:rounded-r-none">
           {/* Content for the right side div goes here */}
         </div>
       </div>
