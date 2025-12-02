41c809eb9cf18160ab1c3fa5afb0da0fa0d5c55d
Aryan Ranjan <ashu13112003@gmail.com>
2025-12-02 04:42:28 +0000

    then we discuss is there an way svg itself yk the paths themselves strec

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import { HeroSvg } from "@/components/hero-svg";
 export default function Home() {
   return (
     <main className="flex h-[calc(100vh-3.5rem)] w-full flex-row items-start justify-start bg-background p-0 overflow-hidden">
-      <div className="h-full w-[70%] pr-2" style={{ marginTop: '-120px' }}>
+      <div className="h-full w-[75%]" style={{ marginTop: '-120px' }}>
         <HeroSvg />
       </div>
-      <div className="h-full w-[30%] bg-purple-500 rounded-l-xl">
+      <div className="h-full w-[25%] bg-purple-500 rounded-l-xl">
         {/* Content for the right side div goes here */}
       </div>
     </main>
