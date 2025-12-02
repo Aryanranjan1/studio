6756e3b9bbb80e8d703ae472867891b596784750
Aryan Ranjan <ashu13112003@gmail.com>
2025-12-02 03:44:05 +0000

    too much bring it down 15px

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { HeroSvg } from "@/components/hero-svg";
 export default function Home() {
   return (
     <main className="flex h-[calc(100vh-3.5rem)] w-full flex-row items-start justify-start bg-background p-0 overflow-hidden">
-      <div className="h-full w-[70%]" style={{ marginTop: '-135px' }}>
+      <div className="h-full w-[70%]" style={{ marginTop: '-120px' }}>
         <HeroSvg />
       </div>
       <div className="h-full w-[30%] bg-purple-500 rounded-l-xl">
