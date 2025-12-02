de1ee5794acd3970602ee310cbb754ac26875b4e
Aryan Ranjan <ashu13112003@gmail.com>
2025-12-02 05:47:53 +0000

    perfecto this is perfect for desktop this layout is perfect i need you t

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,17 @@ import { HeroSvg } from '@/components/hero-svg';
 
 export default function Home() {
   return (
-    <main className="flex h-screen w-full flex-row items-stretch justify-start bg-background p-0 overflow-hidden">
-      <div className="w-[75%] flex flex-col">
+    <main className="flex w-full flex-col lg:flex-row items-stretch justify-start bg-background p-0 overflow-hidden lg:h-screen">
+      <div className="w-full lg:w-[75%] flex flex-col">
         {/* SVG Container (grows to fill space) */}
-        <div className="flex-1 overflow-hidden -mt-[92px]">
+        <div className="flex-1 overflow-hidden lg:-mt-[92px]">
           <HeroSvg />
         </div>
 
         {/* Mid Section */}
         <div
           id="mid-section"
-          className="flex items-stretch pl-7 pb-2 mt-2 mr-2"
+          className="flex items-stretch pl-4 pr-4 lg:pl-7 lg:pr-2 pb-2 mt-2"
         >
           <div className="h-32 w-[30%] rounded-lg bg-card">
             {/* Content for the first element */}
@@ -22,7 +22,7 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="w-[25%] rounded-l-xl bg-purple-500">
+      <div className="w-full lg:w-[25%] lg:rounded-l-xl bg-purple-500 h-64 lg:h-auto mt-4 lg:mt-0">
         {/* Content for the right side div goes here */}
       </div>
     </main>
