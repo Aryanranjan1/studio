d1a7ac5b67f728cc051a7854d4fcd48f13ca6e79
Aryan Ranjan <ashu13112003@gmail.com>
2025-12-02 04:55:23 +0000

    decrease hieght ration should ideally be 3:2 max 1:1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,19 @@
-
 import { HeroSvg } from "@/components/hero-svg";
 
 export default function Home() {
   return (
-    <main className="relative flex h-[calc(100vh-3.5rem)] w-full flex-col items-start justify-start bg-background p-0 overflow-hidden">
-      <div className="h-full w-[75%] flex flex-col" style={{ marginTop: '-120px' }}>
-        <HeroSvg />
+    <main className="relative flex h-[calc(100vh-3.5rem)] w-full flex-row items-start justify-start bg-background p-0 overflow-hidden">
+      <div className="h-full w-[75%] flex flex-col">
+        <div style={{ marginTop: '-120px' }}>
+          <HeroSvg />
+        </div>
 
         {/* Mid Section */}
-        <div id="mid-section" className="flex-grow flex items-center p-4">
-            <div className="w-[30%] aspect-square bg-card rounded-lg">
+        <div id="mid-section" className="flex-grow flex items-stretch p-4 h-64">
+            <div className="w-[30%] bg-card rounded-lg">
                 {/* Content for the first element */}
             </div>
-            <div className="flex-1 h-full ml-2 bg-card rounded-lg" style={{ maxHeight: 'calc(30vw)' }}>
+            <div className="flex-1 ml-2 bg-card rounded-lg">
                 {/* Content for the second element */}
             </div>
         </div>
