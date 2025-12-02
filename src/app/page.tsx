874cad9388be15ec8fafde7ee79ec3dbe56c7fd9
Aryan Ranjan <ashu13112003@gmail.com>
2025-12-02 03:37:42 +0000

    now right side div will be called hero right side dive thi will maintain

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import { HeroSvg } from "@/components/hero-svg";
 
 export default function Home() {
   return (
-    <main className="relative flex h-[calc(100vh-3.5rem)] w-full flex-col items-start justify-start bg-background p-0 overflow-hidden">
-      <div className="absolute left-0 top-0 w-full lg:w-[70%] lg:h-[75vh] lg:-mt-[10vh]">
+    <main className="flex h-[calc(100vh-3.5rem)] w-full flex-row items-start justify-start bg-background p-0 overflow-hidden">
+      <div className="h-full w-[70%]">
         <HeroSvg />
       </div>
-      <div className="absolute right-0 top-0 h-full w-[30%] bg-purple-500 rounded-l-xl"></div>
+      <div className="h-full w-[30%] bg-purple-500 rounded-l-xl">
+        {/* Content for the right side div goes here */}
+      </div>
     </main>
   );
 }
