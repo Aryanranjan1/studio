7ec662ec6452155f13623daac8aea815121a1ed2
Aryan Ranjan <ashu13112003@gmail.com>
2025-12-02 05:06:32 +0000

    now there is space but divs arent taking it dive margin should not be mo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { HeroSvg } from "@/components/hero-svg";
 export default function Home() {
   return (
     <main className="relative flex h-[calc(100vh-3.5rem)] w-full flex-row items-start justify-start bg-background p-0 overflow-hidden">
-      <div className="h-full w-[75%] flex flex-col justify-between">
+      <div className="h-full w-[75%] flex flex-col">
         <div style={{ transform: 'translateY(-16%)' }}>
           <HeroSvg />
         </div>
