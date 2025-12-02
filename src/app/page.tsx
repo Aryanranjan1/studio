a0c9254ba4ff6235e3a580781a53acb0f45862da
Aryan Ranjan <ashu13112003@gmail.com>
2025-12-02 05:39:21 +0000

    now mid group right side reduce margin to only 8px

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,12 +12,12 @@ export default function Home() {
         {/* Mid Section */}
         <div
           id="mid-section"
-          className="flex items-stretch pl-7 pb-2 mt-2"
+          className="flex items-stretch pl-7 pb-2 mt-2 mr-2"
         >
           <div className="h-32 w-[30%] rounded-lg bg-card">
             {/* Content for the first element */}
           </div>
-          <div className="ml-2 h-32 flex-1 rounded-lg bg-card mr-8">
+          <div className="ml-2 h-32 flex-1 rounded-lg bg-card">
             {/* Content for the second element */}
           </div>
         </div>
