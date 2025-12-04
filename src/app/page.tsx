b90f49292a466af4662eb9070cd0101814e74244
Aryan Ranjan <ashu13112003@gmail.com>
2025-12-04 08:09:52 +0000

    no instead of incresing dimensions of grid it just added space this wont

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { HeroGrid } from '@/components/hero-grid';
 
 export default function Home() {
   return (
-    <main className="relative flex flex-col p-2 min-h-[calc(100vh-theme(spacing.14))] sm:min-h-screen sm:items-center sm:justify-center">
+    <main className="relative flex flex-col min-h-[calc(100vh-theme(spacing.14))] sm:min-h-screen">
       <HeroGrid />
     </main>
   );
