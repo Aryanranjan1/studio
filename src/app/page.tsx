53080e84d33b5a81c480574e27d4d8cb94bfb90a
Aryan Ranjan <ashu13112003@gmail.com>
2025-12-04 08:07:54 +0000

    didnt work in tabelet it didnt occupy entire screen can you fix that  so

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { HeroGrid } from '@/components/hero-grid';
 
 export default function Home() {
   return (
-    <main className="flex flex-1 flex-col p-2">
+    <main className="relative flex flex-col p-2 min-h-[calc(100vh-theme(spacing.14))] sm:min-h-screen sm:items-center sm:justify-center">
       <HeroGrid />
     </main>
   );
