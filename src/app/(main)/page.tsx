6aa89d76e9743d23b8e03ee67247c3ab8957f42a
Aryan Ranjan <ashu13112003@gmail.com>
2025-12-08 14:58:35 +0000

    add top and bottom margin for her grid section

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         <div className="col-span-12 bg-black">
           <OzonteHero />
         </div>
-        <div className="col-span-12 bg-black">
+        <div className="col-span-12 bg-black my-16">
           <HeroGrid />
         </div>
         <div className="col-span-12 bg-black">
