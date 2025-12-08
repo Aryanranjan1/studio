69f9283c71ba9ea88ccd91cea733c263243f5af0
Aryan Ranjan <ashu13112003@gmail.com>
2025-12-08 08:07:58 +0000

    division lines make itr more white so its clear

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -159,7 +159,7 @@ export default function BlogPage() {
           <div className="col-span-12 bg-black">
             <div className="container mx-auto px-4 sm:px-6 lg:px-8">
               <section>
-                <div className="grid grid-cols-1 gap-px bg-neutral-800 md:grid-cols-2 lg:grid-cols-3">
+                <div className="grid grid-cols-1 gap-px bg-neutral-700 md:grid-cols-2 lg:grid-cols-3">
                   {filteredArticles.map((article) => (
                     <Link href={`/blog/${article.id}`} key={article.id} className="group" data-event="ArticleCardClick">
                       <div className="h-full overflow-hidden bg-black p-4 transition-all duration-300 group-hover:bg-neutral-900">
