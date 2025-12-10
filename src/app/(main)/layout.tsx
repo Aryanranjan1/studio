1cd46fec0834e0e09e8b65d4f2a876ca0157d58e
Aryan Ranjan <ashu13112003@gmail.com>
2025-12-10 09:32:21 +0000

    in mobile version a empty side vbar is present can you remove it

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -8,7 +8,7 @@ export default function MainLayout({
   return (
     <div className="relative flex min-h-screen flex-col bg-background">
       <Header />
-      <main className="flex-1 pl-20">{children}</main>
+      <main className="flex-1 md:pl-20">{children}</main>
     </div>
   );
 }
