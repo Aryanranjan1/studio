0fa5be82355e8c389f6ff913a3472721fddeea9a
Aryan Ranjan <ashu13112003@gmail.com>
2026-01-12 12:48:20 +0000

    fix: Correctly implement preconnect hints

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,8 +55,9 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="preconnect" href="https://ampire-studio-92664092-32a02.firebaseapp.com" />
-        <link rel="preconnect" href="https://www.googleapis.com" />
+        <link rel="preconnect" href="https://ampire-studio-92664092-32a02.firebaseapp.com" crossOrigin="anonymous" />
+        <link rel="preconnect" href="https://www.googleapis.com" crossOrigin="anonymous" />
+        <link rel="preconnect" href="https://apis.google.com" crossOrigin="anonymous" />
       </head>
       <body className={cn('antialiased', inter.variable, spaceGrotesk.variable)} suppressHydrationWarning={true}>
         <LenisProvider>
